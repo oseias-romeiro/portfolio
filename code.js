efaebabc64326484e723b5372fabcf69e1bbf301
oseias-romeiro <oseiasromeirom@gmail.com>
2022-02-18 21:21:14 -0300

    fixes

--- code.js
+++ code.js
@@ -15,30 +15,16 @@ window.addEventListener("load", periodo);
 
 meLeva = (onde) => {
     // scroll
-    if(screen.width < 640 ){
-        if(onde == "sobre"){
-            window.scrollTo(0, 764);
-        }
-        else if(onde == "projetos") {
-            window.scrollTo(0, 1540);
-        }
-        else if(onde == "contato") {
-            window.scrollTo(0, 2180);
-        }
+    if(onde == "sobre"){
+        window.scrollTo(0, 764);
+    }
+    else if(onde == "projetos") {
+        window.scrollTo(0, 1540);
+    }
+    else if(onde == "contato") {
+        window.scrollTo(0, 2180);
+    }
+    if(screen.width < 900 ){
         document.getElementById("btnMobile").click();
-    }else {
-        document.getElementById(onde).scrollIntoView();
-    }
-
-    // active current nav-item
-    let item = document.getElementsByClassName("nav-link");
-    for(let i=0; i<4; i++){
-        item[i].className = "nav-link";
-    }
-    for(let i=0; i<4; i++){
-        if( (item[i].innerHTML).toUpperCase() === onde.toUpperCase()){
-            item[i].className = "nav-link active";
-            break;
-        }
     }
 }
