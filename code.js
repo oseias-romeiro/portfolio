9e0138cb2b35312b422e3351e1a915c93f6debea
oseias-romeiro <oseiasromeirom@gmail.com>
2022-05-13 22:17:51 -0300

    menu items update

--- code.js
+++ code.js
@@ -14,14 +14,42 @@ periodo = ()=>{
 window.addEventListener("load", periodo);
 
 meLeva = (onde) => {
+    let btnsMenus = document.getElementsByTagName("button");
+    let btnsLink = document.getElementsByTagName("a");
+
+    // standard class buttons
+    btnsMenus[1].className = "nav-link";
+    btnsMenus[2].className = "nav-link";
+    btnsMenus[3].className = "nav-link";
+    btnsLink[1].className = "nav-link";
+
+    // standard style buttons
+    btnsMenus[1].style = "";
+    btnsMenus[2].style = "";
+    btnsMenus[3].style = "";
+    btnsLink[1].style = "";
+
+
+
     // scroll
     if(onde == "sobre"){
+        btnsMenus[1].className = "nav-link active";
+        btnsMenus[1].style = "text-decoration: underline;";
         window.scrollTo(0, 764);
     }
     else if(onde == "projetos") {
+        btnsMenus[2].className = "nav-link active";
+        btnsMenus[2].style = "text-decoration: underline;";
         window.scrollTo(0, 1540);
     }
     else if(onde == "contato") {
+        btnsMenus[3].className = "nav-link active";
+        btnsMenus[3].style = "text-decoration: underline;";
+        window.scrollTo(0, 2180);
+    }
+    else if(onde == "home") {
+        btnsLink[1].className = "nav-link active";
+        btnsLink[1].style = "text-decoration: underline;";
         window.scrollTo(0, 2180);
     }
     if(screen.width < 900 ){
