ba1e80d300f57a92c75c8cea0fb6267819a47da3
oseias-romeiro <oseiasromeirom@gmail.com>
2022-05-14 01:21:41 -0300

    react animation

--- react.js
+++ react.js
@@ -1,5 +1,36 @@
-const Teste = ()=>{
-    return <p>teste</p>
+const { useState } = React
+
+let spaceLeft = 500;
+if(screen.width < 900 ){
+    spaceLeft = 0;
+}
+
+const Arrow = ()=>{
+    const [pos, setPos] = useState(200);
+    setTimeout(()=>{
+        if(pos <= 0){
+            setPos(() => 200);
+        }else {
+            setPos(() => pos-10);
+        }
+    }, 100)
+
+    return (
+        <div className="row" style={{marginLeft: spaceLeft, textalign: "center"}}>
+            <div className="col">
+                <img src="images/arrow-up.png" height="50" alt="arrow-up" style={{marginTop: pos+100}}/>
+                <img src="images/arrow-up.png" height="50" alt="arrow-up" style={{marginTop: pos}}/>
+            </div>
+            <div className="col">
+                <img src="images/arrow-up.png" height="50" alt="arrow-up" style={{marginTop: pos}}/>
+                <img src="images/arrow-up.png" height="50" alt="arrow-up" style={{marginTop: pos+50}}/>
+            </div>
+            <div className="col">
+                <img src="images/arrow-up.png" height="50" alt="arrow-up" style={{marginTop: pos}}/>
+                <img src="images/arrow-up.png" height="50" alt="arrow-up" style={{marginTop: pos+100}}/>
+            </div>
+        </div>
+    )
 }
 const arrows = ReactDOM.createRoot(document.getElementById("arrows"));
-arrows.render(<Teste />);
+arrows.render(<Arrow />);
