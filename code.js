02a92b871bd0c3f2fb88738d9b39ee53fe8e7e55
oseias-romeiro <oseiasromeirom@gmail.com>
2022-07-24 01:12:17 -0300

    updates

--- code.js
+++ code.js
@@ -33,17 +33,17 @@ meLeva = (onde) => {
     if(onde == "sobre"){
         btnsMenus[1].className = "nav-link active";
         btnsMenus[1].style = "text-decoration: underline;";
-        window.scrollTo(0, 764);
+        window.scrollTo(0, 1050);
     }
     else if(onde == "projetos") {
         btnsMenus[2].className = "nav-link active";
         btnsMenus[2].style = "text-decoration: underline;";
-        window.scrollTo(0, 1540);
+        window.scrollTo(0, 2040);
     }
     else if(onde == "contato") {
         btnsMenus[3].className = "nav-link active";
         btnsMenus[3].style = "text-decoration: underline;";
-        window.scrollTo(0, 2180);
+        window.scrollTo(0, 3030);
     }
     else if(onde == "home") {
         btnsLink[1].className = "nav-link active";
